7cf0d9de4cde54d1199b6d39eabeb16ec60829ea
Paul Badour <paulrbadour@gmail.com>
2023-10-25 00:12:40 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import discord # Discord.py
+from dotenv import load_dotenv # python-dotenv
+import os
+
+
+dir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(dir, 'token.env'))
+dtok = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = discord.Client(intents=intents)
+
+
+
+
+@bot.event
+
+async def on_ready():
+    print("Ready")
+
+
+
+@bot.event
+
+async def on_message(message):
+    print(message)
+    if message.content == "yo":
+        await message.channel.send("Hey retard")
+
+
+bot.run(dtok)
